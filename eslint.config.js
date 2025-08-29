006e89ce5ddfc1d73bfa3aa8b12a8a48fdd9bc78
ElysiaK423 <xsx3276627633@163.com>
2025-08-29 13:36:36 +0800

    start

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,32 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginVue from 'eslint-plugin-vue'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
+  ...pluginVue.configs["flat/essential"],
 
   {
-    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      }
-    }
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
   },
 
   {
-    files: ['**/*.vue'],
+    files: ["**/*.vue"],
     languageOptions: {
       parserOptions: {
-        parser: tseslint.parser
-      }
-    }
+        parser: tseslint.parser,
+      },
+    },
   },
 
-  { ignores: ['dist', 'node_modules', '*.d.ts'] }
-)
+  { ignores: ["dist", "node_modules", "*.d.ts"] },
+);
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+const i = {
+  tset:
+    "iii",
+  try:
+    "123",
+};
+console.log(
+  i);
 </script>
 
 <template>
