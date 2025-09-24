594c0ee3e58c3296853cc27446875a0fee477962
ElysiaK423 <xsx3276627633@163.com>
2025-09-24 19:28:53 +0800

    完成商品属性分类查询，问题:后端接口200但data:null；可能是没有数据在里面，建议做添加模块，在做查询模块

--- src/api/product/attr/attr.ts
+++ src/api/product/attr/attr.ts
@@ -2,7 +2,21 @@ import http from "@/utils/http";
 import type { ResponseDataType } from "./type";
 const API = {
   GET_1_CATEGORY: "/admin/product/getCategory1",
+  ///admin/product/getCategory2/{id}
+  GET_2_CATEGORY: "/admin/product/getCategory2",
+  GET_3_CATEGORY: "/admin/product/getCategory3",
+  //获取对应分类的属性
+  GET_ATTR_INFO_LIST: "/admin/product/attrInfoList",
 };
 export const reqCategory1 = () => {
   return http.get<object, ResponseDataType>(API.GET_1_CATEGORY);
+};
+export const reqCategory2 = (id: number) => {
+  return http.get<object, ResponseDataType>(API.GET_2_CATEGORY + "/" + id);
+};
+export const reqCategory3 = (id: number) => {
+  return http.get<object, ResponseDataType>(API.GET_3_CATEGORY + "/" + id);
+};
+export const reqAttrInfoList = (id1: number, id2: number, id3: number) => {
+  return http.get(API.GET_ATTR_INFO_LIST + "/" + id1 + "/" + id2 + "/" + id3);
 };
--- src/views/product/attr/attr.vue
+++ src/views/product/attr/attr.vue
@@ -1,37 +1,122 @@
 <script setup lang="ts">
 defineOptions({ name: "product-attr" });
-import { reqCategory1 } from "@/api/product/attr/attr";
+import {
+  reqCategory1,
+  reqCategory2,
+  reqCategory3,
+  reqAttrInfoList,
+} from "@/api/product/attr/attr";
 import { ElMessage } from "element-plus";
 import { onMounted, ref } from "vue";
 
+// let attrInfoList = ref()
+
 //分类
-//一级分类
-let category1s = ref();
-let category1Selection = ref("");
+let category1Selection = ref();
+let category1OptionList = ref();
+let category2Selection = ref(category1Selection.value);
+let category2OptionList = ref();
+let category3Selection = ref(category2Selection.value);
+let category3OptionList = ref();
+
+//初始化一级分类未选择，二三级分类被ban
+let isC2Dsiabled = ref(true);
+let isC3Dsiabled = ref(true);
+
 onMounted(async () => {
   //在进入页面立即获取一级分类
   const category1Res = await reqCategory1();
-  category1s.value = category1Res.data;
-  console.log(category1Res);
+
   if (category1Res.code !== 200) {
     ElMessage({
       type: "error",
-      message: "获取分类失败，请检查网络",
+      message: "获取一级分类失败，请检查网络",
     });
+  } else {
+    //得到一级分类
+    category1OptionList.value = category1Res.data;
   }
 });
+//一级分类改变
+const get2nd = async () => {
+  //清空和解锁
+  //先清空二级三级分类内容
+  category2Selection.value = null;
+  category3Selection.value = null;
+  //解锁二级分类
+  isC2Dsiabled.value = false;
+  //ban掉三级选择
+  isC3Dsiabled.value = true;
+
+  //业务逻辑
+  //确认存在
+  if (category1Selection.value) {
+    //发送请求获取二级分类
+    const c2Res = await reqCategory2(category1Selection.value);
+    if (c2Res.code !== 200) {
+      ElMessage({
+        type: "error",
+        message: "获取二级分类失败，请检查网络",
+      });
+    } else {
+      category2OptionList.value = c2Res.data;
+    }
+  }
+};
+//二级分类改变
+const get3nd = async () => {
+  //解锁三级分类
+  isC3Dsiabled.value = false;
+  //先清空三级分类内容
+  category3Selection.value = null;
+
+  if (category1Selection.value && category2Selection.value) {
+    //发送请求获取三级分类
+    const c3Res = await reqCategory3(category2Selection.value);
+    if (c3Res.code !== 200) {
+      ElMessage({
+        type: "error",
+        message: "获取二级分类失败，请检查网络",
+      });
+    } else {
+      category3OptionList.value = c3Res.data;
+    }
+  }
+};
+//三级分类改变
+const getProAttr = async () => {
+  if (
+    category1Selection.value &&
+    category2Selection.value &&
+    category3Selection.value
+  ) {
+    const infoListRes = await reqAttrInfoList(
+      category1Selection.value,
+      category2Selection.value,
+      category3Selection.value,
+    );
+    console.log("infoListRes", infoListRes);
+  }
+};
+
+//添加属性
+const addNewAttr = () => {};
 </script>
 
 <template>
-  <el-card style="max-width: 95%">
-    <!-- 分类 -->
+  <!-- 分类 -->
+  <el-card style="max-width: 95%; margin-bottom: 3vh">
     <div class="categorys">
       <!-- 一级分类 -->
       <div class="category">
         <span class="category-title">一级分类：</span>
-        <el-select v-model="category1Selection">
+        <el-select
+          v-model="category1Selection"
+          @change="get2nd"
+          placeholder="请选择一级分类"
+        >
           <el-option
-            v-for="item in category1s"
+            v-for="item in category1OptionList"
             :label="item.name"
             :key="item.id"
             :value="item.id"
@@ -42,19 +127,54 @@ onMounted(async () => {
       <!-- 二级分类 -->
       <div class="category">
         <span class="category-title">二级分类：</span>
-        <el-select>
-          <!-- <el-option></el-option> -->
+        <el-select
+          v-model="category2Selection"
+          @change="get3nd"
+          :disabled="isC2Dsiabled"
+          :placeholder="
+            category1Selection ? '请选择二级分类' : '请选择一级分类'
+          "
+        >
+          <el-option
+            v-for="item in category2OptionList"
+            :label="item.name"
+            :key="item.id"
+            :value="item.id"
+          ></el-option>
         </el-select>
       </div>
 
       <!-- 三级分类 -->
       <div class="category">
         <span class="category-title">三级分类：</span>
-        <el-select>
-          <!-- <el-option></el-option> -->
+        <el-select
+          v-model="category3Selection"
+          @change="getProAttr"
+          :disabled="isC3Dsiabled"
+          :placeholder="
+            category2Selection
+              ? '请选择三级分类'
+              : category1Selection
+                ? '请选择二级分类'
+                : '请选择一级分类'
+          "
+        >
+          <el-option
+            v-for="item in category3OptionList"
+            :label="item.name"
+            :key="item.id"
+            :value="item.id"
+          ></el-option>
         </el-select>
       </div>
     </div>
+  </el-card>
+
+  <!-- 展示 -->
+  <el-card style="max-width: 95%; height: 75vh">
+    <el-button @click="addNewAttr">添加属性</el-button>
+    <!-- <el-table :data="attrInfoList" stripe style="width: 100%">
+    </el-table> -->
   </el-card>
 </template>
 
